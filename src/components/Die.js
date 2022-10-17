b42e37d550585bf3d66f47d1a03f726bb0ac9c51
ShiftyShifter <58483864+ShiftyShifter@users.noreply.github.com>
2022-10-17 22:31:16 +0300

    improved UI also added rolls counting tag

--- src/components/Die.js
+++ src/components/Die.js
@@ -2,7 +2,8 @@ import React from "react";
 
 export default function Die(props) {
     const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
+        backgroundColor: props.isHeld ? "#1796fd" : "transparent",
+        color: props.isHeld ? "white" : "#1796fd"
     }
     return (
         <div className="die-face" style={styles} onClick={props.holdDice}>
